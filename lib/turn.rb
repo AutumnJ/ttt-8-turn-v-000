c3d0f41127c33695585f6e3776a5c0d098f26048
Autumn Jung <autumnkangas@gmail.com>
2018-01-04 16:14:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,7 +5,7 @@ end
 def valid_move(board, index)
 #return true if the index is within the correct range of 0-8 and is currently unoccupied by an X or O token.
 #may want to define a position taken method
-end  
+end
 
 def move(board, index, token = "X")
 #should set the correct index value of that position within the board equal to the token.
